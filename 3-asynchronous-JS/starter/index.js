4e37e3e3bb1cc98c90f9846ad53cf3a22b7ea482
Angel Ivanov <angel.iwanov@gmail.com>
2023-10-24 12:56:49 +0300

    async await

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -19,17 +19,81 @@ const writeFilePro = (file, data) => {
   });
 };
 
-readFilePro(`${__dirname}/dog.txt`)
-  .then((data) => {
+//ASYNC PROMISSE
+// readFilePro(`${__dirname}/dog.txt`)
+//   .then((data) => {
+//     console.log(`Breed ${data}`);
+//     return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+//   })
+//   .then((res) => {
+//     console.log(res.body.message);
+//     return writeFilePro("dog-img.txt", res.body.message);
+//     // fs.writeFile("dog-img.txt", res.body.message, (err) => {
+//     //   if (err) return console.log(err.message);
+//     // });
+//   })
+//   .then(() => console.log(`Dog image saved to file`))
+//   .catch((err) => console.log(err));
+
+//ASYNC AWAIT
+const getDogPic = async () => {
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`);
     console.log(`Breed ${data}`);
-    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
-  })
-  .then((res) => {
-    console.log(res.body.message);
-    return writeFilePro("dog-img.txt", res.body.message);
-    // fs.writeFile("dog-img.txt", res.body.message, (err) => {
-    //   if (err) return console.log(err.message);
-    // });
-  })
-  .then(() => console.log(`Dog image saved to file`))
-  .catch((err) => console.log(err));
+    // const res = await superagent.get(
+    //   `https://dog.ceo/api/breed/${data}/images/random`
+    // );
+
+    // AWAITING 3 PROMISES
+    const res1 = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    const res2 = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    const res3 = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    const all = await Promise.all([res1, res2, res3]);
+    const imgs = all.map((el) => el.body.message);
+    console.log(imgs);
+
+    // console.log(res.body.message);
+    // await writeFilePro("dog-img.txt", res.body.message);
+    await writeFilePro("dog-img.txt", imgs.join("\n"));
+    console.log("FILE SUCCESSFULLY CREATED");
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+  return "2: READY";
+};
+
+getDogPic();
+
+//ASYNC AWAIT
+// (async () => {
+//   try {
+//     console.log("1: GET DOG PICS");
+//     const x = await getDogPic();
+//     console.log(x);
+//     console.log("3: DONE");
+//   } catch (err) {
+//     console.log(err);
+//   }
+// })();
+
+//ASYNC PROMISE
+// getDogPic()
+//   .then((x) => {
+//     console.log("1: GET DOG PICS");
+//     console.log(x);
+//     console.log("3: DONE");
+//   })
+//   .catch((err) => console.log(err));
+
+//SYNC
+// console.log("1: GET DOG PICS");
+// const x = getDogPic();
+// console.log(x);
+// console.log("3: DONE");
