edf8b3caad668ab7ee5ab91b8a344af5508a27c3
Angel Ivanov <angel.iwanov@gmail.com>
2023-10-24 19:17:20 +0300

    middleware routing

---
+++ 4-natours/starter/app.js
@@ -0,0 +1,104 @@
+const express = require('express');
+const fs = require('fs');
+const morgan = require('morgan');
+
+const app = express();
+
+//MIDDLEWARE
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+//ROU
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+const gelAllTours = (req, res) => {
+  res.status(200).json({
+    message: 'success',
+    reqestedAt: req.requestTime,
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
+};
+
+const getTour = (req, res) => {
+  const id = Number(req.params.id);
+  if (id > tours.length) {
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+  }
+
+  const tour = tours.find((tour) => tour.id === id);
+
+  res.status(200).json({
+    message: 'success',
+    data: {
+      tour: tour,
+    },
+  });
+};
+
+const createTour = (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({ status: 'success', data: { tour: newTour } });
+    }
+  );
+};
+
+const updateTour = (req, res) => {
+  const id = Number(req.params.id);
+  if (id > tours.length) {
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: 'Updated tour here...',
+    },
+  });
+};
+
+const deleteTour = (req, res) => {
+  const id = Number(req.params.id);
+  if (id > tours.length) {
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+  }
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
+
+// app.get('/api/v1/tours', gelAllTours);
+// app.get('/api/v1/tours/:id', getTour);
+// app.post('/api/v1/tours', createTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
+
+app.route('/api/v1/tours').get(gelAllTours).post(createTour);
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
