2ece137ad2fe6b34e59db786eb7f7cfefa0146ed
Angel Ivanov <angel.iwanov@gmail.com>
2023-10-27 19:35:18 +0300

    mongoDb mongoose

--- 4-natours/starter/routes/tourRoutes.js
+++ 4-natours/starter/routes/tourRoutes.js
@@ -3,12 +3,18 @@ const tourController = require('../controllers/tourController');
 
 const router = express.Router();
 
-router.param('id', tourController.checkId);
+router
+  .route('/top-5-cheap')
+  .get(tourController.aliasTopTours, tourController.gelAllTours);
+
+router.route('/tour-stats').get(tourController.getTourStats);
+
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
 router
   .route('/')
   .get(tourController.gelAllTours)
-  .post(tourController.checkBody, tourController.createTour);
+  .post(tourController.createTour);
 router
   .route('/:id')
   .get(tourController.getTour)
