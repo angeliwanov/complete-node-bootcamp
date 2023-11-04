61fa3282dba8c080b63140a444b2c34bbb51135f
Angel Ivanov <angel.iwanov@gmail.com>
2023-11-04 16:34:17 +0200

    overview tour templates

--- 4-natours/starter/controllers/viewController.js
+++ 4-natours/starter/controllers/viewController.js
@@ -12,8 +12,16 @@ exports.getOverview = catchAsync(async (req, res) => {
   });
 });
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour',
+exports.getTour = catchAsync(async (req, res) => {
+  //1) Get the tour data, including reviews and guides
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
   });
-};
+  //2) Build template`
+  //3) Render the template
+  res.status(200).render('tour', {
+    title: tour.name,
+    tour,
+  });
+});
