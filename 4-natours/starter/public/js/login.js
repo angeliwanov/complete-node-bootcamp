dd899169e09354d60ba51bb5c7bc160d9b3e4026
Angel Ivanov <angel.iwanov@gmail.com>
2023-11-05 17:25:50 +0200

    login.js

--- 4-natours/starter/public/js/login.js
+++ 4-natours/starter/public/js/login.js
@@ -1,5 +1,17 @@
-const login = (email, password) => {
-  console.log(email, password);
+const login = async (email, password) => {
+  try {
+    const res = await axios({
+      mehod: 'POST',
+      url: 'http://127.0.0.1:3000/api/v1/users/login',
+      data: {
+        email,
+        password,
+      },
+    });
+    console.log(res);
+  } catch (err) {
+    console.log(err.response.data);
+  }
 };
 
 document.querySelector('.form').addEventListener('submit', (e) => {
