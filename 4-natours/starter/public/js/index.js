c68d63da30f8cbc27a417db55e4a7378cf8271f5
Angel Ivanov <angel.iwanov@gmail.com>
2023-11-06 20:26:03 +0200

    update_user_data_API_HTML

--- 4-natours/starter/public/js/index.js
+++ 4-natours/starter/public/js/index.js
@@ -1,12 +1,13 @@
 import '@babel/polyfill';
 import { displayMap } from './mapbox';
 import { login, logout } from './login';
+import { updateData } from './updateSettings';
 
 //DOM ELEMENTS
 const mapBox = document.getElementById('map');
-const loginForm = document.querySelector('.form');
+const loginForm = document.querySelector('.form-login');
 const logoutBtn = document.querySelector('.nav__el--logout');
-
+const updateDataForm = document.querySelector('.form-user-data');
 //DELEGATION
 if (mapBox) {
   const locations = JSON.parse(mapBox.dataset.locations);
@@ -23,3 +24,12 @@ if (loginForm) {
 }
 
 if (logoutBtn) logoutBtn.addEventListener('click', logout);
+
+if (updateBtn) {
+  updateDataForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    updateData(name, email);
+  });
+}
