f7e50104f263799bcb6d7797db7efab14cafe93d
angeliwanov <angel.iwanov@gmail.com>
2023-11-06 09:34:38 +0200

    isLoggedIn

--- 4-natours/starter/controllers/authController.js
+++ 4-natours/starter/controllers/authController.js
@@ -105,6 +105,29 @@ exports.protect = catchAsync(async (req, res, next) => {
   next();
 });
 
+//ONLY FOR RENDERED PAGES<, NO ERRORS
+exports.isLoggedIn = catchAsync(async (req, res, next) => {
+  if (req.cookies.jwt) {
+    //1. Verify token
+    const decoded = await promisify(jwt.verify)(
+      req.cookies.jwt,
+      process.env.JWT_SECRET,
+    );
+
+    //2. Check is user still exists
+    const currentUser = await User.findById(decoded.id);
+    if (!currentUser) next();
+
+    //3. Check if user changed password after JWT was issued
+    if (currentUser.changedPasswordAfter(decoded.iat)) next();
+
+    //There is logged in User
+    res.locals.user = currentUser;
+    next();
+  }
+  next();
+});
+
 exports.restrictTo =
   (...roles) =>
   (req, res, next) => {
