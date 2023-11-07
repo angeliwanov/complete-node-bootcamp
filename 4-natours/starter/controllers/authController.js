5727da34cdd0caa46eedd052394070d6da32ed25
Angel Ivanov <angel.iwanov@gmail.com>
2023-11-07 16:50:41 +0200

    welcome email with pug

--- 4-natours/starter/controllers/authController.js
+++ 4-natours/starter/controllers/authController.js
@@ -4,7 +4,7 @@ const crypto = require('crypto');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
-const sendEmail = require('../utils/email');
+const Email = require('../utils/email');
 
 const signToken = (id) =>
   jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -46,6 +46,10 @@ exports.signup = catchAsync(async (req, res, next) => {
     role: req.body.role,
     photo: req.body.photo,
   });
+
+  const url = `${req.protocol}://${req.get('host')}/me`;
+  console.log(url);
+  await new Email(newUser, url).sendWelcome();
 
   createSendToken(newUser, 201, res);
 });
@@ -177,11 +181,11 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   const message = `Forgot you password? Submit a PATCH request with your password and passwordConfirm to ${resetURL}. \n If you didn't, please ignore this email!`;
 
   try {
-    await sendEmail({
-      email: user.email,
-      subject: 'Your password reset token (valid for 10 min)',
-      message,
-    });
+    // await sendEmail({
+    //   email: user.email,
+    //   subject: 'Your password reset token (valid for 10 min)',
+    //   message,
+    // });
 
     res.status(200).json({
       status: 'success',
--- 4-natours/starter/utils/email.js
+++ 4-natours/starter/utils/email.js
@@ -1,26 +1,61 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
+const path = require('path');
 
-const sendEmail = async (options) => {
-  //1) Create a transporter
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Angel Ivanov <${process.env.EMAIL_FROM}>`;
+  }
 
-  //2 Dafine the email options
-  const mailOptions = {
-    from: 'Angel Ivanov',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-  };
+  newTransport() {
+    if (process.env.NODE_ENV === 'production ') {
+      //Sendgird
+      return 1;
+    }
 
-  //3.Actually sent the email
-  await transporter.sendMail(mailOptions);
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  async send(template, subject) {
+    //1) Render HTML based on a pug template
+    // C:\Users\A475231\complete-node-bootcamp\4-natours\starter\views\email\welcome.pug
+    const filePath = path.join(
+      __dirname,
+      '..',
+      'views',
+      'email',
+      `${template}.pug`,
+    );
+    const html = pug.renderFile(filePath, {
+      firstName: this.firstName,
+      url: this.url,
+      subject,
+    });
+
+    //2) Define email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      html,
+      text: htmlToText.fromString(html),
+    };
+
+    //3) Create a transport and send email
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to the Natours family');
+  }
 };
-
-module.exports = sendEmail;
